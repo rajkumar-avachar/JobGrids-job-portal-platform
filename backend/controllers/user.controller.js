e6b0f66237ae515e65a9d45b9c8fd77d99653315
rajkumar-avachar <rajavachar59@gmail.com>
2026-03-01 13:30:46 +0530

    test

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -124,7 +124,7 @@ export const login = async (req, res) => {
     };
 
     const token = jwt.sign(tokenData, process.env.JWT_SECRET, {
-      expiresIn: "7d",
+      expiresIn: "1d",
     });
 
     const userData = {
@@ -143,7 +143,7 @@ export const login = async (req, res) => {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
         sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-        maxAge: 7 * 24 * 60 * 60 * 1000,
+        maxAge: 1 * 60 * 60 * 1000,
       })
       .json({
         message: `Welcome back ${user.fullname}`,
